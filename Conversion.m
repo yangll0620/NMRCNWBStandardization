dbfb16823ad1b5f7b7beecdd3d201d3df25ef3fc
yangll0620 <yll0620@gmail.com>
2019-03-04 17:43:24 -0600

    adding raw data

--- Conversion.m
+++ Conversion.m
@@ -46,6 +46,20 @@ nwb.general_experiment_description = 'Habit trail experiment'; % General descrip
 nwb.file_create_date = date(); 
 nwb.session_start_time = datetime(dateOfExp, 'InputFormat','yyyyMMdd');% required, Date and time of the experiment/session start. COMMENT: - The date is stored in UTC with local timezone offset as ISO 8601 extended formatted string: 2018-09-28T14:43:54.123+02:00 - Dates stored in UTC end in "Z" with no timezone offset. - Date accuracy is up to milliseconds.
 
+
+%% device information
+ADCName = 'TDT (132 channels)';
+nwb.general_devices.set( ADCName, types.core.Device());
+arrayName = 'Gray Matter';
+nwb.general_devices.set( arrayName, types.core.Device());
+
+egroup = types.core.ElectrodeGroup(...
+    ''
+    );
+
+%% raw data
+
+    
 %% export
 outdest = fullfile(outloc, [identifier '.nwb']);
 nwbExport(nwb, outdest);
--- Conversion_TutorialExpDataset.m
+++ Conversion_TutorialExpDataset.m
@@ -1,5 +1,5 @@
 clear
-%% matnwb path
+%% matnwb path for windows or mac notebook
 if ispc % PC (Windows) in LRB 411
     dropboxpath = 'E:'; 
 end
@@ -13,25 +13,133 @@ datasetpath = fullfile(projectpath, 'dataset', 'TutorialExpData'); %.....\NWBtes
 
 %% Script start here
 addpath(matnwbpath)
+
 %% script configuration
 animal = 'ANM255200';
 session = '20140910';
 
 identifier = [animal '_' session];
 
-metadata_loc = fullfile(datasetpath, 'metadata', ['meta_data_' identifier '.mat']);
-datastructure_loc = fullfile(datasetpath,'data_structure_files',...
+downloadpath = fullfile(datasetpath, 'downloaddata');
+metadata_loc = fullfile(downloadpath,'metadata', ['meta_data_' identifier '.mat']);
+datastructure_loc = fullfile(downloadpath,'data_structure_files',...
     ['data_structure_' identifier '.mat']);
-rawdata_loc = fullfile(datasetpath, 'rawdata');
+rawdata_loc = fullfile(downloadpath, [identifier '.tar']);
 
 outloc = fullfile(datasetpath, 'out');
 
-%%
-nwb = nwbfile();
-
-%% raw data
 if 7 ~= exist(outloc, 'dir')
     mkdir(outloc);
 end
 
-rawfiles = dir(fullfile(rawdata_loc, identifier));
+source_file = [mfilename() '.m'];
+[~, source_script, ~] = fileparts(source_file);
+
+%% General Information
+nwb = nwbfile();
+nwb.identifier = identifier;
+
+loaded = load(metadata_loc, 'meta_data');
+meta = loaded.meta_data;
+
+%experiment-specific treatment for animals with the ReaChR gene modification
+isreachr = any(cell2mat(strfind(meta.animalGeneModification, 'ReaChR')));
+
+% Device objects are essentially just a list of device names.  
+% probe hardware name
+probetype = meta.extracellular.probeType{1};
+probeSource = meta.extracellular.probeSource{1};
+deviceName = [probetype ' (' probeSource ')'];
+nwb.general_devices.set(deviceName,...
+    types.core.Device());
+
+if isreachr
+    laserName = 'laser-594nm (Cobolt Inc., Cobolt Mambo 100)';
+else
+    laserName = 'laser-473nm (Laser Quantum, Gem 473)';
+end
+nwb.general_devices.set(laserName, types.core.Device());
+% use the formatStruct function which is provided in the tutorials directory
+addpath(fullfile(matnwbpath, 'tutorials')); 
+
+recordingLocation = meta.extracellular.recordingLocation{1};
+egroup = types.core.ElectrodeGroup(...
+    'device', types.untyped.SoftLink(['/general/devices/' deviceName]));
+    
+nwb.general_extracellular_ephys.set(deviceName, egroup);
+
+egroupPath = ['/general/extracellular_ephys/' deviceName];
+etrodeNum = length(meta.extracellular.siteLocations);
+etrodeMat = cell2mat(meta.extracellular.siteLocations .');
+emptyStr = repmat({''}, etrodeNum,1);
+dtColNames = {'x', 'y', 'z', 'imp', 'location', 'filtering','group',...
+    'group_name'};
+% you can specify column names and values as key-value arguments in the DynamicTable
+% constructor.
+dynTable = types.core.DynamicTable(...
+    'colnames', dtColNames,...
+    'description', 'Electrodes',...
+    'id', types.core.ElementIdentifiers('data', int64(1:etrodeNum)),...
+    'x', types.core.VectorData('data', etrodeMat(:,1),...
+        'description', 'the x coordinate of the channel location'),...
+    'y', types.core.VectorData('data', etrodeMat(:,2),...
+        'description', 'the y coordinate of the channel location'),...
+    'z', types.core.VectorData('data', etrodeMat(:,3),...
+        'description','the z coordinate of the channel location'),...
+    'imp', types.core.VectorData('data', zeros(etrodeNum,1),...
+        'description','the impedance of the channel'),...
+    'location', types.core.VectorData('data',...
+        repmat({recordingLocation}, etrodeNum, 1),...
+        'description', 'the location of channel within the subject e.g. brain region'),...
+    'filtering', types.core.VectorData('data', emptyStr,...
+        'description', 'description of hardware filtering'),...
+    'group', types.core.VectorData('data',...
+        repmat(types.untyped.ObjectView(egroupPath), etrodeNum, 1),...
+        'description', 'a reference to the ElectrodeGroup this electrode is a part of'),...
+    'group_name', types.core.VectorData('data', repmat({probetype}, etrodeNum, 1),...
+        'description', 'the name of the ElectrodeGroup this electrode is a part of'));
+
+nwb.general_extracellular_ephys_electrodes = dynTable;
+
+%% raw acquisition data
+untarLoc = fullfile(downloadpath,identifier);
+if 7 ~= exist(untarLoc, 'dir')
+    untar(rawdata_loc, downloadpath);
+end
+rawfiles = dir(untarLoc);
+rawfiles = fullfile(untarLoc, {rawfiles(~[rawfiles.isdir]).name});
+
+nrows = length(nwb.general_extracellular_ephys_electrodes.id.data);
+tablereg = types.core.DynamicTableRegion(...
+    'description','Relevent Electrodes for this Electrical Series',...
+    'table',types.untyped.ObjectView('/general/extracellular_ephys/electrodes'),...
+    'data',(1:nrows) - 1);
+objrefs = cell(size(rawfiles));
+trials_idx = nwb.intervals_trials;
+endTimestamps = trials_idx.start_time.data;
+
+for i = 1: length(rawfiles)
+    tnumstr = regexp(rawfiles{i}, '_trial_(\d+)\.mat$', 'tokens', 'once');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
+
