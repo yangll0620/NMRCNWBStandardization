498e06c7103979ac83483847b2434c6ee815d840
yangll0620 <yll0620@gmail.com>
2019-03-05 16:55:16 -0600

    update test.m

--- test.m
+++ test.m
@@ -1,11 +1,4 @@
-clear 
 
-%% test video files
-videofile_avi = 'BugHabittrail-181130_Block-1_Cam2.avi';
-videofile_mp4 = 'CPB10_113018.mp4';
 
-obj_avi = VideoReader(videofile_avi);
-obj_mp4 = VideoReader(videofile_mp4);
 
-frame_avi = readFrame(obj_avi);
 
