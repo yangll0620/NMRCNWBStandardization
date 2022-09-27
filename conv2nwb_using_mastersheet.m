434de93f7f3ac7ff7c7ae53b7adb136a1302832c
yangwingitbash <yll0620@gmail.com>
2022-09-27 15:30:27 -0400

    tdtrawpath to char type

--- conv2nwb_using_mastersheet.m
+++ conv2nwb_using_mastersheet.m
@@ -1,10 +1,10 @@
-function conv2nwb_using_mastersheet(googleSheetID, sheet_name, driver, date, block)
+function conv2nwb_using_mastersheet(googleSheetID, sheet_name, driver, dateofexp, block)
 % 
 %   using master sheet to convert data to NWB structure
 %
 %   Example usage:
 %       
-%       conv2nwb_using_mastersheet('1ITkEPoIkQDr1RebfI4BtUdNK8UNbl8Bvpx-XbvH-v68', 'MasterList', 'Z:')
+%       conv2nwb_using_mastersheet('1ITkEPoIkQDr1RebfI4BtUdNK8UNbl8Bvpx-XbvH-v68','MasterList', 'Z:', '031722', 1);
 %       
 %Inputs:
 %
@@ -14,7 +14,7 @@ function conv2nwb_using_mastersheet(googleSheetID, sheet_name, driver, date, blo
 %
 %   driver: the driver name for network drive (e.g Z:)
 %
-%   date: the exact date of the files (mmddyy)
+%   dateofexp: the exact date of the files (mmddyy)
 %
 %   block: the block number
 
@@ -24,7 +24,7 @@ tbl = webread(url_name);
 [nrow,~] = size(tbl);
 
 for i = 1 : nrow
-    if contains(tbl.OutputFolderName(i),date) && (tbl.TDTBlock(1)==block)
+    if contains(tbl.OutputFolderName(i),dateofexp) && (tbl.TDTBlock(i)==block)
 
         % proceed only when the it has been cleaned
         folderName = string(tbl.OutputFolderName(i));
@@ -38,10 +38,11 @@ for i = 1 : nrow
             if isunix
                 rawtdtpath = strrep(rawtdtpath, '\', '/');
             end
-            rawtdtpath = fullfile(driver, rawtdtpath);
+            rawtdtpath = convertStringsToChars(fullfile(driver, rawtdtpath));
         
             %get identifer by extracting info
             identifier = strcat(folderName,'_TDTbk',num2str(tbl.TDTBlock(i)),'_',string(tbl.BriefDescription(i))); %identifier for nwb file, formatted
+            identifier = convertStringsToChars(identifier);
         
             %get animal name from identifier
             parse_id = strsplit(identifier,'_');
@@ -50,29 +51,24 @@ for i = 1 : nrow
         
             %put tdt info into nwb by tdt filepath
             tdt = TDTbin2mat(rawtdtpath);
-        
-        
+            
             nwb = convraw_tdt2nwb(tdt, 'nwb_in', nwb, 'animal', animal); % change the animal name accordingly
-        
-
-
-            if(strcmp(tbl.MACleanedFileName(i),'_cleaned') && strcmp(tbl.ANCExported_,'ANC exported'))
+            
+            if(strcmp(tbl.MACleanedFileName(i),'_cleaned') & strcmp(tbl.ANCExported_,'ANC exported'))
                 
                 %full path for MA anc and trc_cleaned files on the server
                 MApath = strcat(tbl.rawMAfolder(i),'\',tbl.MASessionFolder(i),'\',tbl.MASessionName,'_',num2str(tbl.MAFile(i)));
                 rawancfile = strcat(MApath, '.anc');
                 rawtrcfile = strcat(MApath,'_cleaned.trc');
-
+                
                 if isunix
-                        rawancfile = strrep(rawancfile, '\', '/');
-                        rawtrcfile = strrep(rawtrcfile, '\', '/');
+                    rawancfile = strrep(rawancfile, '\', '/');
+                    rawtrcfile = strrep(rawtrcfile, '\', '/');
                 end
         
                 %put MA info into nwb by file path
                 nwb = convraw_ma2nwb(rawancfile, rawtrcfile, 'nwb_in', nwb, 'identifier', identifier);
             end
-
-
 
             EyeTracking = tbl.EyeTracking(i);
             if(~isempty(EyeTracking{1}))
@@ -81,20 +77,20 @@ for i = 1 : nrow
                 EyeTPath = strcat('root2\Animals2\Barb\Recording\Raw\','Barb Eyetracking\',tbl.EyeTracking(i),'.txt');
 
                 if isunix
-                        EyeTPath = strrep(EyeTPath, '\', '/');
+                    EyeTPath = strrep(EyeTPath, '\', '/');
                 end
-        
+                
                 %put MA info into nwb by file path
                 nwb = convprocessed_eye2nwb(EyeTPath, 'nwb_in', nwb, 'identifier', identifier);
             end
-        
-                
-                %export derived nwb
-                out_filename = strcat(identifier,'.nwb');
-                outNwbFile = fullfile(outcodepath,  'NMRCNWB_TestData', out_filename);
-                disp(['...Exporting NWB file to ' outNwbFile ' ...'])
-                nwbExport(nwb, outNwbFile);
-        
+            
+            
+            %export derived nwb
+            out_filename = strcat(identifier,'.nwb');
+            outNwbFile = fullfile(outcodepath,  'NMRCNWB_TestData', out_filename);
+            disp(['...Exporting NWB file to ' outNwbFile ' ...'])
+            nwbExport(nwb, outNwbFile);
+            
         end
     end
 end
